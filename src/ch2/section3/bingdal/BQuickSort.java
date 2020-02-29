ae485c5fb1f6c4363fb5b584b9eae227422a45c5
chaenabi <find1086@gmail.com>
2020-02-29 13:47:02 +0900

    study chapter2_3 implements BQuickSort

--- src/ch2/section3/bingdal/BQuickSort.java
+++ src/ch2/section3/bingdal/BQuickSort.java
@@ -3,18 +3,29 @@ package ch2.section3.bingdal;
 import ch2.section1.bingdal.BSortTemplate;
 import edu.princeton.cs.algs4.StdRandom;
 
+import java.util.Collections;
+
 public class BQuickSort  extends BSortTemplate {
 
     public void sort(Comparable[] a) {
         StdRandom.shuffle(a);
         sort(a, 0, a.length-1);
     }
-
+    static int i = 0;
     private static void sort(Comparable[] a, int lo, int hi) {
+
+        System.out.print(++i +"번째 소트: ");
+        for(Comparable s : a) {
+            System.out.print(s + " ");
+        }
+        System.out.println("\n-----------------------------------------------");
         if(hi <= lo) return;
+
         int j = partition(a, lo, hi);
         sort(a, lo, j-1);
         sort(a, j+1, hi);
+
+
     }
 
     private static int partition(Comparable[] a, int lo, int hi){
@@ -29,12 +40,16 @@ public class BQuickSort  extends BSortTemplate {
         exch(a, lo, j);
         return j;
 
+
     }
+
 
     public static void main(String[] args) {
         BQuickSort bQuickSort = new BQuickSort();
-        Comparable[] comparables = {1,2,3,4};
+        Comparable[] comparables = {"A","R","U","T","E","L","E","P","K","I","M","Q","C","X","O","S"};
         bQuickSort.sort(comparables);
+
+
     }
 
 }
